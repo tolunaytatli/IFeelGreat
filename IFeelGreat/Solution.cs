0ecb25d338d8208c219b556a35381c29a127a209
tolunaytatli <tolunaytatli@gmail.com>
2021-04-07 13:45:54 +0300

    65% of the users is male while 35% is female. Male users always respond with a “No” while female users respond with either a “Yes” or “No” in equal numbers. We have just received a “No” response from a user. What is the probability that this user is a male? (Diffrent input values allowed)

---
+++ IFeelGreat/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFeelGreat
+{
+    public class  Solution
+    {
+        public double maleRatio;
+        public double femaleRatio;
+        public double maleYes;
+        public double maleNo;
+        public double femaleYes;
+        public double femaleNo;
+
+        public Solution(double _maleRatio,double _maleYes,double _femaleYes)
+        {
+
+            maleRatio = _maleRatio;
+            maleYes = _maleYes;
+            femaleYes = _femaleYes;
+           
+            femaleRatio = 1- maleRatio;
+            maleNo = 1 - maleYes;
+            femaleNo = 1 - femaleYes;
+            
+
+        }
+        public void probabilityNo()
+        {   //                           
+            double probabilityMaleNo = (maleRatio * maleNo) / ((maleRatio * maleNo) + (femaleRatio * femaleNo));
+            double probabilityFemaleNo = (femaleRatio * femaleNo) / ((maleRatio * maleNo) + (femaleRatio * femaleNo));
+            
+            Console.WriteLine("No/Male Probability: " + String.Format("{0:0.00}",probabilityMaleNo));
+            Console.WriteLine("No/Female Probability: " + String.Format("{0:0.00}", probabilityFemaleNo));
+        }
+
+        public void probabilityYes()
+        {
+            double probabilityMaleYes = (maleRatio * maleYes) / ((maleRatio * maleYes) + (femaleRatio * femaleYes));
+            double probabilityFemaleYes = (femaleRatio * femaleYes) / ((maleRatio * maleYes) + (femaleRatio * femaleYes));
+           
+            Console.WriteLine("Yes/Male Probability: " + String.Format("{0:0.00}",probabilityMaleYes));
+            Console.WriteLine("Yes/Female Probability: " + String.Format("{0:0.00}",probabilityFemaleYes));
+        }
+
+
+    }
+}
